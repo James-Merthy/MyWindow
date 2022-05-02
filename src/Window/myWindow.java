a28f44f1d5b24eafd85d4581972a3d14f9966f1d
james <anasala.merthy@gmail.com>
2022-05-02 13:58:42 +0200

    MainWindow cree

--- src/Window/myWindow.java
+++ src/Window/myWindow.java
@@ -1,30 +1,35 @@
 package Window;
 
 import javax.swing.*;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 import java.awt.*;
 
 public class myWindow extends JFrame {
 
-    myWindow(){
+
+    myWindow() {
 
         super("First Swing ");
 
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);// Dispose On close libere toute les recousres de la fenêtre ...
-        this.setSize(600,400); // tille de la window a la création
-       // this.setLocation(100,500);  position de la window
+        this.setSize(600, 400); // tille de la window a la création
+        // this.setLocation(100,500);  position de la window
         this.setLocationRelativeTo(null);// centre de l'ecran
-        JPanel contentPane  = (JPanel) this.getContentPane();
+        JPanel contentPane = (JPanel) this.getContentPane();
         contentPane.setLayout(new FlowLayout(FlowLayout.LEFT)); // mettre les elem de la window en ligne
         contentPane.add(new JButton("Push"));// rajouter un boutton
         contentPane.add(new JButton("click"));
         contentPane.add(new JCheckBox("coch moi "));// rajoute une case a cocher
-        contentPane.add( new JTextField ("edit me ")); // zone de texte
+        contentPane.add(new JTextField("ecrire ... ")); // zone de texte
 
     }
 
 
+    public static void main(String[] args) throws Exception{
 
-    public static void main(String[] args) {
+        // Apply look and feel
+
+        UIManager.setLookAndFeel(new NimbusLookAndFeel()); // changer de look
 
         myWindow window = new myWindow();
         window.setVisible(true);
