0ff11c2b093ca00cc7e4302f2657ddd209dfca1c
james <anasala.merthy@gmail.com>
2022-05-02 13:04:13 +0200

    init

---
+++ src/Window/myWindow.java
@@ -0,0 +1,20 @@
+package Window;
+
+import javax.swing.*;
+
+public class myWindow extends JFrame {
+
+    myWindow(){
+
+        super("First Swing ");
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("init");
+
+    }
+}
