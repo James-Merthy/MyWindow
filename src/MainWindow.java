9c4c1c9504a3efc14a897c2c47feb68a5efca104
james <anasala.merthy@gmail.com>
2022-05-02 14:23:40 +0200

    MainWindow2

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+
 public class MainWindow {
+    private JPanel Hello;
+    private JButton pushButton;
+    private JButton clickButton;
+    private JCheckBox caseCheckBox;
+    private JTextField textField1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
